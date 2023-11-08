c7a0c95ec6da8f6f176f67223a7632176dc3a161
Philip Haynes <Philip.haynes@me.com>
2023-11-08 15:16:28 +1100

    Initial checkin

---
+++ add_users.sh
@@ -0,0 +1,11 @@
+USER_EMAIL="bill@example.com"
+USER_PASS="Password1!"
+USER_NAME="bill"
+docker exec test_mm bash -c 'until mmctl --local system status 2> /dev/null; do echo "waiting for server to become available"; sleep 5; done'
+docker exec test_mm mmctl --local team create --name botsample display_name "Sample Bot playground" --email "admin@example.com"
+docker exec test_mm mmctl --local user create --email="${USER_EMAIL}" --password="${USER_PASS}" --username="${USER_NAME}"
+docker exec test_mm mmctl --local roles system_admin bill
+docker exec test_mm mmctl --local team users add botsample bill
+
+echo "Default user credentials"
+echo "Email: ${USER_EMAIL} - Password: ${USER_PASS}"
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/mattermost/mattermost-bot-sample-golang
+
+go 1.15
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/klauspost/compress v1.15.2 // indirect
+	github.com/mattermost/ldap v3.0.4+incompatible // indirect
+	github.com/mattermost/mattermost-server/v6 v6.7.2
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/rs/zerolog v1.15.0
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+)
